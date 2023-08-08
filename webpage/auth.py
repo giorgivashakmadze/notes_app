2020fb8d1b74654b9aeacc331963724cb167a77d
giorgi vashakmadze <128330426+giorgivashakmadze@users.noreply.github.com>
2023-08-08 19:46:28 +0400

    registration route updated, message flashing function added ( dismiss button currently not working)

--- webpage/auth.py
+++ webpage/auth.py
@@ -1,15 +1,41 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    info = request.form
+    print(info)
     return render_template("login.html", text='testing')
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirm_password')
+
+        if not email or not username or not password or not confirm_password:
+            flash('All fields are required', category='error')
+        elif len(email) < 4 or '@' not in email:
+            flash('Email is too short or does not include "@" symbol', category='error')
+        elif len(username) < 2:
+            flash('Username too short', category='error')
+        elif len(password) < 6:
+            flash('Password must be at least 6 characters long', category='error')
+        elif password != confirm_password:
+            flash('Passwords do not match', category='error')
+        else:
+            # Add users to the database (your registration logic here)
+            flash('Registration successful! You can now log in.', category='success')
+            return redirect(url_for('auth.login'))
+
+    return render_template('register.html')
+
 
 @auth.route('/logout')
 def logout():
     return "<h1>logout</h1>"
-
-@auth.route('/register')
-def register():
-    return render_template("register.html")

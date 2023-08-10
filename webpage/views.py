34b030bf966e6b80bb59def20cb89bfbc5398949
giorgi vashakmadze <128330426+giorgivashakmadze@users.noreply.github.com>
2023-08-10 22:34:07 +0400

    shows saved note on index page

--- webpage/views.py
+++ webpage/views.py
@@ -1,10 +1,23 @@
-from flask import Blueprint,render_template
-from flask_login import  current_user, login_required
+from flask import Blueprint, render_template, request, flash
+from flask_login import current_user, login_required
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
+    if request.method == 'POST':
+        note_content = request.form.get('note_content')  # Correct form field name
+
+        if note_content:
+            new_note = Note(content=note_content, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note saved', 'success')
+        else:
+            flash('Note content cannot be empty.', 'error')
+
     return render_template("index.html", user=current_user)
